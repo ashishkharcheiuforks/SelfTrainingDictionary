a4dd01d1a6c2b8c3bb44ff024cadd3c3865fcf0a
Назаров Александр Сергеевич <mr.a.nazarov@yandex.ru>
2020-01-18 21:49:05 +0300

    first commit

---
+++ app/src/main/java/com/san4o/just4fun/selftrainingdictionary/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.san4o.just4fun.selftrainingdictionary
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
